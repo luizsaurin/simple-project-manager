b82fd17345c4c3dacdae7b55dec172e6a436b28a
luizsaurin <bladerunner9040@gmail.com>
2025-01-06 17:00:51 -0300

    chore: updated project structure

---
+++ src/model/project.ts
@@ -0,0 +1,11 @@
+import { ProjectStatus } from "../enum/projectStatus.js";
+
+export class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people: number,
+    public status: ProjectStatus
+  ) {}
+}
